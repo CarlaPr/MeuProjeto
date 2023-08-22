0f00d92d1bc3bfe0677f7b225bf2a0af8e6680e0
carlapr <carlaperola.823@outlook.com>
2023-08-22 20:44:54 -0300

    Meu projeto em java

--- src/App.java
+++ src/App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Salve");
+        System.out.println("Aprendendo Java na DIO");
     }
 }
