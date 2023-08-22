1c0853e730fb511fa3cd58420a76e6e95886c50b
carlapr <carlaperola.823@outlook.com>
2023-08-22 20:51:23 -0300

    Testando Java

--- src/App.java
+++ src/App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Aprendendo Java na DIO");
+        System.out.print("Aprendendo Java na DIO");
     }
 }
