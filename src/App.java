e8aa807735a729749cb7d74984c095a071a196eb
carlapr <carlaperola.823@outlook.com>
2023-08-23 10:47:14 -0300

    Matriz 2.0

--- src/App.java
+++ src/App.java
@@ -1,5 +1,20 @@
+import java.util.Scanner;
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.print("Aprendendo Java na DIO");
+       
+        int [][] matriz = new int[3] [3];
+        Scanner s = new Scanner(System.in);
+        
+        for (int i=0; i<3; i++){
+            for (int j=0; j<3; j++){
+                System.out.print(" Digite um numero: ");
+                matriz[i] [j] = s.nextInt();
+            }
+        }
+        for (int i=0; i<3; i++){
+            for (int j=0; j<3; j++){
+                System.out.println(matriz[i] [j]);
+            }
+        }
     }
 }
